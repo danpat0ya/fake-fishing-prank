48510e70412a766db4e5ee6871f8448a35fd956c
danpat0ya <139097020+danpat0ya@users.noreply.github.com>
2025-11-17 22:50:45 +0900

    Update static file serving to include HTML file
    Serve 'is_it_real_Fishing.html' as static content.

--- fake-fishing-prank/index.js
+++ fake-fishing-prank/index.js
@@ -3,7 +3,7 @@ const path = require('path');
 const app = express();
 const port = process.env.PORT || 8000;
 
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'public', 'is_it_real_Fishing.html')));
 
 app.get('/healthz', (req, res) => {
   res.status(200).send('OK');
@@ -11,4 +11,5 @@ app.get('/healthz', (req, res) => {
 
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
+
 });
