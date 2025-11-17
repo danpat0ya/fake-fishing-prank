872da4577be64e593808579fe362fcfb883722ee
danpat0ya <139097020+danpat0ya@users.noreply.github.com>
2025-11-17 22:54:02 +0900

    Serve HTML file from root route

--- fake-fishing-prank/index.js
+++ fake-fishing-prank/index.js
@@ -3,7 +3,11 @@ const path = require('path');
 const app = express();
 const port = process.env.PORT || 8000;
 
-app.use(express.static(path.join(__dirname, 'public', 'is_it_real_Fishing.html')));
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'is_it_real_Fishing.html'));
+});
 
 app.get('/healthz', (req, res) => {
   res.status(200).send('OK');
@@ -11,5 +15,4 @@ app.get('/healthz', (req, res) => {
 
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
-
 });
